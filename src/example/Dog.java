ca410e4c238c34ec013b3b9fdf992093332e6f2c
kim-shun <kimura1417080@gmail.com>
2021-07-25 08:41:37 +0900

    Dogクラスを作成

---
+++ src/example/Dog.java
@@ -0,0 +1,29 @@
+package example;
+
+public class Dog implements Pet {
+
+	@Override
+	public void eat() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void eat(String food) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void playToy() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void playToy(String toy) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
