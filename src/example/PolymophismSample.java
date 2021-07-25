277e5f6f41db0ab233b469b143d0376de5c0dbe4
kim-shun <kimura1417080@gmail.com>
2021-07-25 09:03:28 +0900

    ポリモーフィズムのメリットを活用したアプリの作成

---
+++ src/example/PolymophismSample.java
@@ -0,0 +1,18 @@
+package example;
+
+import java.util.Scanner;
+
+public class PolymophismSample {
+
+	public static void main(String[] args) {
+		Pet pet;
+		
+		System.out.println("触れ合いたいペットを選んでください");
+		System.out.println("  1:猫、2:犬 (1 or 2 どちらかを入力してEnter):");
+		
+		Scanner keyInput = new Scanner(System.in);
+		String inputStr = keyInput.nextLine();
+
+	}
+
+}
