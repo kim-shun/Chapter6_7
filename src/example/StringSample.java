613292b9d8b49c90d0b8aef6b6ec1a57bcc52aab
kim-shun <kimura1417080@gmail.com>
2021-07-26 14:03:00 +0900

    文字列の文字数を調べる

---
+++ src/example/StringSample.java
@@ -0,0 +1,17 @@
+package example;
+
+public class StringSample {
+
+	public static void main(String[] args) {
+		String str1 = "123";
+		System.out.println("str1 = " + str1);
+		System.out.println("str1.length() → " + str1.length());
+		
+		System.out.println("----------------------");
+		String str2 = "あいう";
+		System.out.println("str2 = " + str2);
+		System.out.println("str2.length() → " + str2.length());
+
+	}
+
+}
