876bfbadffee4edf20d8d86a34232d6e689ed58d
kim-shun <kimura1417080@gmail.com>
2021-07-29 23:28:44 +0900

    ラッパークラスを使う

---
+++ src/example/WrapperClassSample.java
@@ -0,0 +1,13 @@
+package example;
+
+public class WrapperClassSample {
+
+	public static void main(String[] args) {
+		String str1 = "123";
+		int i1 = Integer.parseInt(str1);
+		System.out.println("str1 = " + str1);
+		System.out.println("Integer.parseInt(str1) → " + i1);
+
+	}
+
+}
