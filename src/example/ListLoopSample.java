b398842f2685e29b609426938123a62090e0ef18
kim-shun <kimura1417080@gmail.com>
2021-07-30 17:47:05 +0900

    リストに要素を追加する

---
+++ src/example/ListLoopSample.java
@@ -0,0 +1,17 @@
+package example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListLoopSample {
+
+	public static void main(String[] args) {
+		List<Integer> numberList = new ArrayList<>();
+		
+		numberList.add(100);
+		numberList.add(65);
+		numberList.add(80);
+
+	}
+
+}
