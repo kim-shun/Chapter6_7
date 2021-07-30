802ea33fb7c123fdaddd2894c0fd8d43e5f89d37
kim-shun <kimura1417080@gmail.com>
2021-07-30 16:46:56 +0900

    リストから1つずつ要素を取り出す

--- src/example/ListSample.java
+++ src/example/ListSample.java
@@ -10,6 +10,11 @@ public class ListSample {
 		numberList.add(100);
 		numberList.add(65);
 		numberList.add(80);
+		
+		 System.out.println(numberList.get(0));
+		 System.out.println(numberList.get(1));
+		 System.out.println(numberList.get(2));
+		 
 
 	}
 
