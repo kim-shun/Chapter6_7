cead4dccb595c3c53979146265f0027c2ec88caa
kim-shun <kimura1417080@gmail.com>
2021-07-31 14:34:12 +0900

    マップを使う準備をする

---
+++ src/example/MapSample.java
@@ -0,0 +1,13 @@
+package example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapSample {
+
+	public static void main(String[] args) {
+		Map<String, Integer> fruitsMap = new HashMap<>();
+
+	}
+
+}
