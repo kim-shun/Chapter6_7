ba480834f6c2b4cd5ed3e2b3efb1c474d4b23054
kim-shun <kimura1417080@gmail.com>
2021-07-31 14:35:58 +0900

    マップに要素を追加する

--- src/example/MapSample.java
+++ src/example/MapSample.java
@@ -7,6 +7,11 @@ public class MapSample {
 
 	public static void main(String[] args) {
 		Map<String, Integer> fruitsMap = new HashMap<>();
+		
+		fruitsMap.put("りんご", 100);
+		fruitsMap.put("みかん", 150);
+		fruitsMap.put("ばなな", 250);
+		
 
 	}
 
