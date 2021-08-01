83f7b5c25e683ea6cf8b2ef2ea2cce2aa6b35041
kim-shun <kimura1417080@gmail.com>
2021-08-01 10:56:29 +0900

    データ型にPet型を指定してHashMapを生成する

---
+++ src/example/MapSample2.java
@@ -0,0 +1,17 @@
+package example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapSample2 {
+
+	public static void main(String[] args) {
+		Map<String, Pet> petMap = new HashMap<>();
+		
+		Pet pet1 = new Cat();
+		Pet pet2 = new Dog();
+		
+
+	}
+
+}
